5d0a2af52cfb691040fe45faf9a41845d1419b72
temialonge <temitopealonge6@gmail.com>
2023-02-17 02:32:08 +0100

    ham

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const mainMenu = document.querySelector(".mainMenu");
+
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active")
-    mainMenu.classList.toggle("active")
+    hamburger.classList.toggle("active");
+    mainMenu.classList.toggle("active");
 })
+
+document.querySelectorAll(".mainMenu").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    mainMenu.classList.remove("active");
+}))
